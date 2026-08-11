512451c48c5565be689a9b40430119e825bf8207
sarthakmulik <sarthakmulik@users.noreply.github.com>
2026-08-11 16:37:12 +0530

    fix: Allow Next.js middleware to bypass all manifest.json files

--- src/middleware.ts
+++ src/middleware.ts
@@ -29,6 +29,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|manifest.json|sw.js|api/|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|manifest.*\\.json|sw.js|api/|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
